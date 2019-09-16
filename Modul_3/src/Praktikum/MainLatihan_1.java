941b04009afe1e3cd35127424dcb8dc38a5729de
AbdulQoheng <abdulqoheng@gmail.com>
2019-09-16 08:00:29 +0700

    upload Modul 3

---
+++ Modul_3/src/Praktikum/MainLatihan_1.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum;
+
+/**
+ *
+ * @author qoheng
+ */
+public class MainLatihan_1 {
+    public static void main(String[] args) {
+        Latihan_1 ob = new Latihan_1();
+        
+        int bilangan[] = {-5,-3,-6,-3,-4};
+        ob.setBilangan(bilangan);
+        ob.cetak("Bilangan = ");
+        ob.cetakbilangan(ob.getBilangan());
+        ob.setMax(bilangan);
+        ob.cetak("Nilai Max = "+ob.getMax());
+        ob.setMin(bilangan);
+        ob.cetak("Nilai Min = "+ob.getMin());
+        ob.setRatarata(bilangan);
+        ob.cetak("Nilai Rata-Rata = "+ob.getRatarata());
+        ob.setCari(-3);
+        ob.cetak("Nilai -3 di Index = ");
+        ob.cetakindex(ob.getCari());
+        ob.cetak("");
+        
+
+//        int i;
+//        Max=bilangan[0];
+//        for (i = 0; i<5;i++ ){
+//            
+//            if (Max < bilangan[i]){
+//                Max = bilangan[i];
+//                System.out.println(Max);
+////                System.out.println("asdfa");
+//            }
+//        }
+        
+
+        
+    }
+    
+}
